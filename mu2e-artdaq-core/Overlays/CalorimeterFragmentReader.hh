011ad62b5f79acb4f83ae3c8235cd68ca66f5eb1
Tomonari Miyashita <tomo@caltech.edu>
2018-08-30 17:02:57 -0500

    Updated includes to use cetlib_except/exception.h instead of cetlib/exception.h

--- mu2e-artdaq-core/Overlays/CalorimeterFragmentReader.hh
+++ mu2e-artdaq-core/Overlays/CalorimeterFragmentReader.hh
@@ -2,7 +2,7 @@
 #define mu2e_artdaq_core_Overlays_CalorimeterFragmentReader_hh
 
 #include "artdaq-core/Data/Fragment.hh"
-#include "cetlib/exception.h"
+#include "cetlib_except/exception.h"
 
 #include "mu2e-artdaq-core/Overlays/DetectorFragment.hh"
 
