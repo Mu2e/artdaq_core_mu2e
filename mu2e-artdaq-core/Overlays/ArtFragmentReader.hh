523b1979179a72e8e750d23f96cba90e42468746
Tomonari Miyashita <tomo@caltech.edu>
2019-07-16 18:49:35 -0500

    Added updated CAL accessor methods to ArtFragmentReader.hh. Now supports multi-hit DataBlocks.

--- mu2e-artdaq-core/Overlays/ArtFragmentReader.hh
+++ mu2e-artdaq-core/Overlays/ArtFragmentReader.hh
@@ -52,15 +52,21 @@ public:
 	std::array<adc_t, 15> DBT_Waveform(adc_t const *pos);
 	adc_t DBT_Flags(adc_t const *pos);
 
-	// CAL DataBlock Payload Accessor Methods (by block address)
-	adc_t DBC_CrystalID(adc_t const *pos);
-	adc_t DBC_apdID(adc_t const *pos);
-	adc_t DBC_Time(adc_t const *pos);
-	adc_t DBC_NumSamples(adc_t const *pos);
-	adc_t DBC_PeakSampleIdx(adc_t const *pos);
-	std::vector<int> DBC_Waveform(adc_t const *pos);
+	// CAL DataBlock Payload Accessor Method (by block address)
+	adc_t DBCR_NumHits(adc_t const *pos);
 
-	// CRV ROC Status Accessor Methods
+	adc_t DBC_ChannelID(adc_t const *pos, size_t hitIdx);
+	adc_t DBC_BoardID(adc_t const *pos, size_t hitIdx);
+	adc_t DBC_Time(adc_t const *pos, size_t hitIdx);
+	adc_t DBC_NumSamples(adc_t const *pos, size_t hitIdx);
+	adc_t DBC_PeakSampleIdx(adc_t const *pos, size_t hitIdx);
+        std::vector<int> DBC_Waveform(adc_t const *pos, size_t hitIdx);
+
+	adc_t DBC_DIRACOutputA(adc_t const *pos, size_t hitIdx);
+	adc_t DBC_DIRACOutputB(adc_t const *pos, size_t hitIdx);
+	adc_t DBC_ErrorFlags(adc_t const *pos, size_t hitIdx);
+
+	// CRV ROC Status Accessor Methods (by block address)
 	adc_t DBVR_ControllerID(adc_t const *pos);
 	adc_t DBVR_PacketType(adc_t const *pos);
 	adc_t DBVR_EventWordCount(adc_t const *pos);
@@ -210,38 +216,59 @@ mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBT_Flags(adc_t const *p
 // CAL DataBlock Payload Accessor Methods (by block address)
 ////////////////////////////////////////////////////////////////////////////////
 
-mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_CrystalID(adc_t const *pos)
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBCR_NumHits(adc_t const *pos)
 {
-	return *(pos + 8 + 0) & 0x0FFF;  // 0x0FFF = 0b1111 1111 1111
+	return *(pos + 8 + 0);
 }
 
-mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_apdID(adc_t const *pos)
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_ChannelID(adc_t const *pos, size_t hitIdx)
 {
-	return *(pos + 8 + 0) >> 12;
+        return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 0) >> 10;
 }
 
-mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_Time(adc_t const *pos)
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_BoardID(adc_t const *pos, size_t hitIdx)
 {
-	return *(pos + 8 + 1);
+	return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 0) & 0x03FF;
 }
 
-mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_NumSamples(adc_t const *pos)
+
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_Time(adc_t const *pos, size_t hitIdx)
 {
-	return *(pos + 8 + 2) & 0x00FF;
+	return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 1);
 }
 
-mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_PeakSampleIdx(adc_t const *pos)
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_NumSamples(adc_t const *pos, size_t hitIdx)
 {
-	return *(pos + 8 + 2) >> 8;
+	return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 2) & 0x00FF;
 }
 
-std::vector<int> mu2e::ArtFragmentReader::DBC_Waveform(adc_t const *pos)
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_PeakSampleIdx(adc_t const *pos, size_t hitIdx)
 {
-	std::vector<int> waveform(DBC_NumSamples(pos));
+        return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 2) >> 8;
+}
+
+std::vector<int> mu2e::ArtFragmentReader::DBC_Waveform(adc_t const *pos, size_t hitIdx)
+{
+	std::vector<int> waveform(DBC_NumSamples(pos,hitIdx));
 	for (size_t i = 0; i < waveform.size(); i++) {
-		waveform[i] = *(pos + 8 + 3 + i);
+		waveform[i] = *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 3 + i);
 	}
 	return waveform;
+}
+
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_DIRACOutputA(adc_t const *pos, size_t hitIdx)
+{
+        return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 3 + DBC_NumSamples(pos,hitIdx) + 0);
+}
+
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_DIRACOutputB(adc_t const *pos, size_t hitIdx)
+{
+        return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 3 + DBC_NumSamples(pos,hitIdx) + 1);
+}
+
+mu2e::ArtFragmentReader::adc_t mu2e::ArtFragmentReader::DBC_ErrorFlags(adc_t const *pos, size_t hitIdx)
+{
+        return *(pos + 8 + *(pos + 8 + 1 + hitIdx) + 3 + DBC_NumSamples(pos,hitIdx) + 2);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
