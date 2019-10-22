e534df44d99051a86cdbef79a209b3aac3ad1271
Greg <grakness@FNAL.GOV>
2019-10-22 16:32:57 -0500

    changed number of data-blocks-per-fragments

--- mu2e-artdaq-core/Overlays/mu2eFragment.hh
+++ mu2e-artdaq-core/Overlays/mu2eFragment.hh
@@ -10,7 +10,8 @@
 // Implementation of "mu2eFragment", an artdaq::Fragment overlay class
 
 // The "packing factor": How many DataBlocks are stored in each mu2eFragment
-#define DATA_BLOCKS_PER_MU2E_FRAGMENT 2500
+// #define DATA_BLOCKS_PER_MU2E_FRAGMENT 2500
+#define DATA_BLOCKS_PER_MU2E_FRAGMENT 500
 
 namespace mu2e {
 class mu2eFragment;
