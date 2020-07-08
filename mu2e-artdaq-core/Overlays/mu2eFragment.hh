94ad2a34a6ae577bccaeadbd73c0538e6a8297b3
Greg <grakness@FNAL.GOV>
2020-07-08 13:44:52 -0500

    Fix find_ups_root, make Mu2eFragment bigger

--- mu2e-artdaq-core/Overlays/mu2eFragment.hh
+++ mu2e-artdaq-core/Overlays/mu2eFragment.hh
@@ -12,7 +12,7 @@
 // The "packing factor": How many DataBlocks are stored in each mu2eFragment
 // #define DATA_BLOCKS_PER_MU2E_FRAGMENT 2500
 // #define DATA_BLOCKS_PER_MU2E_FRAGMENT 500
-#define DATA_BLOCKS_PER_MU2E_FRAGMENT 5
+#define DATA_BLOCKS_PER_MU2E_FRAGMENT 100
 
 namespace mu2e {
 class mu2eFragment;
