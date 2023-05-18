9e3d554062a3e673968ebdc325ce0ff2eaa1f653
ehrlich-uva <ehrlich@virginia.edu>
2023-05-17 19:25:39 -0500

    Changed the CRVFragments to the newest version of the CRV binary format

--- artdaq-core-mu2e/Overlays/CRVFragment.hh
+++ artdaq-core-mu2e/Overlays/CRVFragment.hh
@@ -3,9 +3,8 @@
 #define MU2E_ARTDAQ_CORE_OVERLAYS_CRVFRAGMENT_HH
 
 #include "artdaq-core-mu2e/Overlays/ArtFragment.hh"
-#include <memory>
 #include <vector>
-#include <array>
+#include <bitset>
 
 namespace mu2e {
 class CRVFragment : public ArtFragment
@@ -36,18 +35,11 @@ public:
 
 		uint16_t TriggerCount;
 
-		uint8_t Status;
-		uint8_t unused3;
+		uint16_t MicroBunchStatus;
 
-		uint8_t unused4;
-		uint8_t unused5;
+		uint16_t EventWindowTag1;
 
-		uint8_t Errors;
-		uint8_t EventType;
-
-                uint16_t MicroBunchNumberLow;
-
-                uint16_t MicroBunchNumberHigh;
+		uint16_t EventWindowTag0;
 
 		CRVROCStatusPacket()
 			: unused1(0)
@@ -59,15 +51,28 @@ public:
 			, ActiveFEBFlags0(0)
 			, ActiveFEBFlags1(0)
 			, TriggerCount(0)
-			, Status(0)
-			, unused3(0)
-			, unused4(0)
-			, unused5(0)
-			, Errors(0)
-			, EventType(0)
-			, MicroBunchNumberLow(0)
-			, MicroBunchNumberHigh(0)
+			, MicroBunchStatus(0)
+			, EventWindowTag1(0)
+			, EventWindowTag0(0)
 		{}
+
+                std::bitset<24> GetActiveFEBFlags() const
+                {
+                  uint32_t ActiveFEBFlags = ActiveFEBFlags2;
+                  ActiveFEBFlags<<=8;
+                  ActiveFEBFlags|=ActiveFEBFlags1;
+                  ActiveFEBFlags<<=8;
+                  ActiveFEBFlags|=ActiveFEBFlags0;
+                  return std::bitset<24>(ActiveFEBFlags);  //only need the 24 lowest bits
+                }
+
+                uint32_t GetEventWindowTag() const
+                {
+                  uint32_t EventWindowTag = EventWindowTag1;
+                  EventWindowTag<<=16;
+                  EventWindowTag|=EventWindowTag0;
+                  return EventWindowTag;
+                }
 	};
 
 	struct CRVHitWaveformSample
@@ -80,24 +85,29 @@ public:
 		{}
 	};
 
-	struct CRVHitReadoutPacket
+	struct CRVHitInfo
 	{
-		uint16_t SiPMID;
+		uint16_t febChannel : 6;
+		uint16_t portNumber : 5;
+		uint16_t controllerNumber : 5;
 
 		uint16_t HitTime : 12;
 		uint16_t NumSamples : 4;
 
-		CRVHitWaveformSample  WaveformSamples[8];
-
-		CRVHitReadoutPacket()
-			: SiPMID(0)
+		CRVHitInfo()
+			: febChannel(0)
+			, portNumber(0)
+			, controllerNumber(0)
 			, HitTime(0)
-			, NumSamples(0) 
+			, NumSamples(0)
                 {}
 	};
 
+        typedef std::vector<CRVHitWaveformSample> CRVHitWaveform;
+        typedef std::pair<CRVHitInfo,CRVHitWaveform> CRVHit;
+
 	std::unique_ptr<CRVROCStatusPacket> GetCRVROCStatusPacket(size_t blockIndex) const;
-	std::vector<CRVHitReadoutPacket> GetCRVHitReadoutPackets(size_t blockIndex) const;
+	std::vector<CRVHit> GetCRVHits(size_t blockIndex) const;
 };
 }  // namespace mu2e
 
