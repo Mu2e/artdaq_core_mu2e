68b485354bfe6348b8869487f69ba51d5880398d
Name <aedmonds@york.cuny.edu>
2023-09-21 20:03:06 -0500

    Committing Pawel's updates to the STMFragment

--- artdaq-core-mu2e/Overlays/STMFragment.hh
+++ artdaq-core-mu2e/Overlays/STMFragment.hh
@@ -2,6 +2,10 @@
 #define MU2E_ARTDAQ_CORE_OVERLAYS_STMFRAGMENT_HH
 
 #include "artdaq-core/Data/Fragment.hh"
+#include "STMDAQ-TestBeam/utils/Hex.hh"
+#include "STMDAQ-TestBeam/utils/dataVars.hh"
+#include "STMDAQ-TestBeam/utils/xml.hh"
+#include "STMDAQ-TestBeam/utils/EnvVars.hh"
 
 namespace mu2e {
 class STMFragment
@@ -9,40 +13,6 @@ class STMFragment
 public:
 	explicit STMFragment(artdaq::Fragment const& f)
 		: artdaq_fragment_(f) {}
-
-	struct STMDataPacket
-	{  // Remeber testbeam data is little endian
-		// uint16_t FixedHeaderWord[16];
-		std::vector<uint16_t> data;
-		STMDataPacket() {}
-	};
-
-	/*
-		struct STMBoardID
-		{
-			uint16_t BoardID : 10;
-			uint16_t ChannelStatusFlagsA : 6;
-			uint16_t ChannelStatusFlagsB : 14;
-			uint16_t unused : 2;
-
-			STMBoardID()
-				: BoardID(0), ChannelStatusFlagsA(0), ChannelStatusFlagsB(0), unused(0) {}
-		};
-
-		struct STMHitReadoutPacket
-		{
-			uint16_t ChannelNumber : 6;
-			uint16_t DIRACA : 10;
-			uint16_t DIRACB;
-			uint16_t ErrorFlags;
-			uint16_t Time;
-			uint8_t NumberOfSamples;
-			uint8_t IndexOfMaxDigitizerSample;
-
-			STMHitReadoutPacket()
-				: ChannelNumber(0), DIRACA(0), DIRACB(0), ErrorFlags(0), Time(0), NumberOfSamples(0), IndexOfMaxDigitizerSample(0) {}
-		};
-	*/
 
 	class STM_tHdr
 	{
@@ -124,7 +94,7 @@ public:
 			T2 = T2 << 32;
 			T3 = T3 << 16;
 			time = T1 | T2 | T3 | T4;
-			return time;
+ 			return time;
 		};
 	};
 
@@ -145,9 +115,108 @@ public:
 		return DataBegin() + GetSHdr()->sliceSize();
 	}
 
-	//	std::unique_ptr<STMBoardID> GetSTMBoardID(size_t blockIndex) const;
-	//	std::vector<std::pair<STMHitReadoutPacket, std::vector<uint16_t>>> GetSTMHits(size_t blockIndex) const;
-	//	std::vector<std::pair<STMHitReadoutPacket, uint16_t>> GetSTMHitsForTrigger(size_t blockIndex) const;
+
+  class STM_fw_pHdr{
+  public:
+    uint16_t _fw_pHdr[fw_pHdr_Len];
+    STM_fw_pHdr(){};
+    uint32_t pNum() const
+    {
+      uint32_t pNum1 = _fw_pHdr[fw_pHdr_pNum1], pNum2 = _fw_pHdr[fw_pHdr_pNum2], pNum;
+      pNum2 = pNum2 >> 16;
+      pNum = pNum2 | pNum1;
+      return pNum;
+    };
+    int16_t test() const
+    {
+      int16_t test = _fw_pHdr[fw_pHdr_end];
+      return test;
+    };
+  };
+
+        class STM_fw_tHdr{
+	public:
+	  uint16_t _fw_tHdr[fw_tHdr_Len];
+	  fw_tHdr fw_tHdr_index;
+	  // TODO: get the indexes of each element from the struct defined in dataVars.hh
+	  STM_fw_tHdr(){};
+
+	  // Defining members of the header;
+	  uint64_t DTCclk() const
+	  {
+	    uint64_t DTCclk0 = _fw_tHdr[fw_tHdr_index.Ch_DTCclk_0], DTCclk1 = _fw_tHdr[fw_tHdr_index.DTCclk_1], DTCclk2 = _fw_tHdr[fw_tHdr_index.DTCclk_2] , DTCclk3 = _fw_tHdr[fw_tHdr_index.DTCclk_3];
+	    DTCclk0 = DTCclk0 & 0xFF00;
+	    DTCclk0 = DTCclk0 >> 8;
+	    DTCclk3 = DTCclk3 << 40;
+	    DTCclk2 = DTCclk2 << 24;
+	    DTCclk1 = DTCclk1 << 8;
+	    DTCclk0 = DTCclk3 | DTCclk2 | DTCclk1 | DTCclk0;
+	    return DTCclk0;
+	  };
+	  
+	  uint64_t EvNum() const
+	  {
+	    uint64_t EvNum0 = _fw_tHdr[fw_tHdr_index.EvNum_0], EvNum1 = _fw_tHdr[fw_tHdr_index.EvNum_1], EvNum2 = _fw_tHdr[fw_tHdr_index.EvNum_2];
+	    EvNum2 = EvNum2 << 32;
+	    EvNum1 = EvNum1 << 16;
+	    EvNum0 = EvNum2 | EvNum1 | EvNum0;
+	    return EvNum0;
+	  };
+
+	  uint64_t ADCclk() const
+	  {
+	    uint64_t ADCclk0 = _fw_tHdr[fw_tHdr_index.ADCclk_0], ADCclk1 = _fw_tHdr[fw_tHdr_index.ADCclk_1], ADCclk2 = _fw_tHdr[fw_tHdr_index.ADCclk_2], ADCclk3 = _fw_tHdr[fw_tHdr_index.ADCclk_3];
+	    ADCclk3 = ADCclk3 << 48;
+	    ADCclk2 = ADCclk2 << 32;
+	    ADCclk1 = ADCclk1 << 16;
+	    ADCclk0 = ADCclk3 | ADCclk2 | ADCclk1 | ADCclk0;
+	    return ADCclk0;
+	  };
+
+	  uint64_t EWT() const 
+	  {
+	    uint64_t EWT0 = _fw_tHdr[fw_tHdr_index.EWT_0], EWT1 = _fw_tHdr[fw_tHdr_index.EWT_1], EWT2 = _fw_tHdr[fw_tHdr_index.EWT_2];
+	    EWT2 = EWT2 << 32;
+	    EWT1 = EWT1 << 16;
+	    EWT0 = EWT2 | EWT1 | EWT0;
+	    return EWT0;
+	  };
+
+	  uint64_t EM() const 
+	  {
+	    uint64_t EM0 = _fw_tHdr[fw_tHdr_index.EM_0], EM1 = _fw_tHdr[fw_tHdr_index.EM_1], EM2 = _fw_tHdr[fw_tHdr_index.EM_2_DRTDC];
+	    EM2 = EM2 & 0xFF00;
+	    EM2 = EM2 << 24;
+	    EM1 = EM1 << 16;
+	    EM0 = EM2 | EM1 | EM0;
+	    return EM0;
+	  };
+
+	  uint16_t DRTDC() const 
+	  {
+	    uint16_t _DRTDC = _fw_tHdr[fw_tHdr_index.EM_2_DRTDC];
+	    _DRTDC = _DRTDC & 0xFF;
+	    return _DRTDC;
+	  };
+
+	  uint16_t EvStart() const
+	  {
+	    uint16_t _EvStart = _fw_tHdr[fw_tHdr_index.EvStart];
+	    return _EvStart;
+	  };
+
+	  uint16_t EvLen() const
+	  {
+	    uint16_t _EvLen = _fw_tHdr[fw_tHdr_index.EvLen];
+	    return _EvLen;
+	  };
+	  uint16_t channel() const 
+	  {
+	    uint16_t _channel = _fw_tHdr[fw_tHdr_index.Ch_DTCclk_0];
+	    _channel = _channel & 0x00FF;
+	    return _channel;
+	  };
+        };
 
 private:
 	artdaq::Fragment const& artdaq_fragment_;
@@ -166,129 +235,5 @@ static constexpr int sw_sHdr_size_bytes = sizeof(STMFragment::STM_sHdr);
 static constexpr int sw_sHdr_size_words = sw_sHdr_size_bytes / 2;
 
 }  // namespace mu2e
-
-/*    uint16_t sw_tHdr[20];
-uint16_t sw_sHdr[8]; //Call this from XML? Worth calling the parameter indexes from XML too?
-bool recombine = false;
-uint32_t sw_tHdr_dataSize = (sw_tHdr_dataSize1 << 16) | sw_tHdr_dataSize2;
-uint32_t sw_tHdr_sliceNumber = (sw_tHdr_sNum1 << 16)| sw_tHdr_sNum2;*/
-
-//    STMDataPacket() : FixedHeaderWord0(0), FixedHeaderWord1(0), FixedHeaderWord2(0), FixedHeaderWord3(0), FixedHeaderWord4(0), FixedHeaderWord5(0), FixedHeaderWord6(0), FixedHeaderWord7(0) { }
-// const std::size_t sw_tHdr_size = 20; // Number of words (uint16_t) in the trigger header.
-// const std::size_t sw_sHdr_size = 8; //Number of words (uint16_t) in the slice header.
-
-/*
-  // Add to the STMDataPacket definition
-  uint16_t sw_tHdr[20];
-  uint16_t sw_sHdr[8]; //Call this from XML? Worth calling the parameter indexes from XML too?
-  bool recombine = false;
-  // Software trigger header members
-  uint16_t sw_tHdr_size = 20; // Number of words (uint16_t) in the trigger header.
-  uint16_t* sw_tHdr_testWord0 = &sw_tHdr[0];
-  uint16_t* sw_tHdr_testWord1 = &sw_tHdr[1];
-  uint16_t* sw_tHdr_dataSize1 = &sw_tHdr[2];
-  uint16_t* sw_tHdr_dataSize2 = &sw_tHdr[3];
-  uint16_t* sw_tHdr_sNum1 = &sw_tHdr[4];
-  uint16_t* sw_tHdr_sNum2 = &sw_tHdr[5];
-  uint16_t* sw_tHdr_TrigNum1 = &sw_tHdr[6];
-  uint16_t* sw_tHdr_TrigNum2 = &sw_tHdr[7];
-  uint16_t* sw_tHdr_MdChTp = &sw_tHdr[8];
-  uint16_t* sw_tHdr_TrigTime1 = &sw_tHdr[9];
-  uint16_t* sw_tHdr_TrigTime2 = &sw_tHdr[10];
-  uint16_t* sw_tHdr_TrigTime3 = &sw_tHdr[11];
-  uint16_t* sw_tHdr_TrigTime4 = &sw_tHdr[12];
-  uint16_t* sw_tHdr_ADCoffset1 = &sw_tHdr[13];
-  uint16_t* sw_tHdr_ADCoffset2 = &sw_tHdr[14];
-  uint16_t* sw_tHdr_DroppedPackets = &sw_tHdr[15];
-  uint16_t* sw_tHdr_unixTime1 = &sw_tHdr[16];
-  uint16_t* sw_tHdr_unixTime2 = &sw_tHdr[17];
-  uint16_t* sw_tHdr_unixTime3 = &sw_tHdr[18];
-  uint16_t* sw_tHdr_unixTime4 = &sw_tHdr[19];
-  uint32_t sw_tHdr_dataSize = (&sw_tHdr_dataSize1 << 16) | &sw_tHdr_dataSize2;
-  uint32_t sw_tHdr_sliceNumber = (&sw_tHdr_sNum1 << 16)| &sw_tHdr_sNum2;
-
-  // Recombine words
-  if(recombine){
-	uint32_t sw_tHdr_testData = (&sw_tHdr_testWord0 << 16) | &sw_tHdr_testWord1;
-	//uint32_t sw_tHdr_dataSize = (&sw_tHdr_dataSize1 << 16) | &sw_tHdr_dataSize2;
-	//uint32_t sw_tHdr_sliceNumber = (&sw_tHdr_sNum1 << 16)| &sw_tHdr_sNum2;
-	uint4_t sw_tHdr_mode = &sw_tHdr_MdChTp & 0b1111000000000000; // Check this
-	uint4_t sw_tHdr_channelNumber = &sw_tHdr_MdChTp & 0b0000111100000000; // Check this
-	uint8_t sw_tHdr_type = &sw_tHdr_MdChTp & 0b0000000011111111; // Check this
-	uint32_t sw_tHdr_ADCOffset = (&sw_tHdr_ADCOffset1 << 16) | &sw_tHdr_ADCOffset2;
-	uint64_t sw_tHdr_unixTime = (&sw_tHdr_unixTime1 << 48) | (&sw_tHdr_unixTime2 << 32) | (&sw_tHdr_unixTime3 << 16) | &sw_tHdr_unixTime4;
-  };
-
-  // Software slice header members
-  uint16_t sw_sHdr_size = 8; // Number of words (uint16_t) in the slice header.
-  uint16_t* sw_sHdr_sNum1 = &sw_sHdr[0];
-  uint16_t* sw_sHdr_sNum2 = &sw_sHdr[1];
-  uint16_t* sw_sHdr_sSize1 = &sw_sHdr[2];
-  uint16_t* sw_sHdr_sSize2 = &sw_sHdr[3];
-  uint16_t* sw_sHdr_ADCtime1 = &sw_sHdr[4];
-  uint16_t* sw_sHdr_ADCtime2 = &sw_sHdr[5];
-  uint16_t* sw_sHdr_ADCtime3 = &sw_sHdr[6];
-  uint16_t* sw_sHdr_ADCtime4 = &sw_sHdr[7];
-  uint32_t sw_sHdr_sliceSize = (&sw_sHdr_sSize1 << 16) | &sw_sHdr_sSize2;
-  // Recombine words
-  if(recombine)}{
-	uint32_t sw_sHdr_sliceNumber = (&sw_sHdr_sNum1 << 16) | &sw_sHdr_sNum2;
-	//uint32_t sw_sHdr_sliceSize = (&sw_sHdr_sSize1 << 16) | &sw_sHdr_sSize2;
-	uint64_t sw_sHdr_ADCtime = (&sw_sHdr_ADCtime1 << 48) | (&sw_sHdr_ADCtime2 << 32) | (&sw_sHdr_ADCtime3 << 16) | &sw_sHdr_ADCtime4;
-  };
-  //uint16_t STMDataPacketSize = 2000; //Each WireShark packet is 2kB.
-  */
-
-/*
-  void update_tHdr_members(STMDataPacket &packet){
-	packet->sw_tHdr_testWord0 = &sw_tHdr[0];
-	packet->sw_tHdr_testWord1 = &sw_tHdr[1];
-	packet->sw_tHdr_dataSize1 = &sw_tHdr[2];
-	packet->sw_tHdr_dataSize2 = &sw_tHdr[3];
-	packet->sw_tHdr_sNum1 = &sw_tHdr[4];
-	packet->sw_tHdr_sNum2 = &sw_tHdr[5];
-	packet->sw_tHdr_TrigNum1 = &sw_tHdr[6];
-	packet->sw_tHdr_TrigNum2 = &sw_tHdr[7];
-	packet->sw_tHr_MdChTp = &sw_tHdr[8];
-	packet->sw_tHdr_TrigTime1 = &sw_tHdr[9];
-	packet->sw_tHdr_TrigTime2 = &sw_tHdr[10];
-	packet->sw_tHdr_TrigTime3 = &sw_tHdr[11];
-	packet->sw_tHdr_TrigTime4 = &sw_tHdr[12];
-	packet->sw_tHdr_ADCoffset1 = &sw_tHdr[13];
-	packet->sw_tHdr_ADCoffset2 = &sw_tHdr[14];
-	packet->sw_tHdr_DroppedPackets = &sw_tHdr[15];
-	packet->sw_tHdr_unixTime1 = &sw_tHdr[16];
-	packet->sw_tHdr_unixTime2 = &sw_tHdr[17];
-	packet->sw_tHdr_unixTime3 = &sw_tHdr[18];
-	packet->sw_tHdr_unixTime4 = &sw_tHdr[19];
-	packet->sw_tHdr_dataSize = (packet->sw_tHdr_dataSize1 << 16) | packet->sw_tHdr_dataSize2;
-	packet->sw_tHdr_sliceNumber = (packet->sw_tHdr_sNum1 << 16)| packet->sw_tHdr_sNum2;
-	if(packet->recombine){
-	  packet->sw_tHdr_testData = (packet->sw_tHdr_testWord0 << 16) | packet->sw_tHdr_testWord1;
-	  //packet->sw_tHdr_dataSize = (packet->sw_tHdr_dataSize1 << 16) | packet->sw_tHdr_dataSize2;
-	  //packet->sw_tHdr_sliceNumber = (packet->sw_tHdr_sNum1 << 16)| packet->sw_tHdr_sNum2;
-	  packet->sw_tHdr_mode = packet->sw_tHdr_MdChTp & 0b1111000000000000; // Check this
-	  packet->sw_tHdr_channelNumber = packet->sw_tHdr_MdChTp & 0b0000111100000000; // Check this
-	  packet->sw_tHdr_type = packet->sw_tHdr_MdChTp & 0b0000000011111111; // Check this
-	  packet->sw_tHdr_ADCOffset = (packet->sw_tHdr_ADCOffset1 << 16) | packet->sw_tHdr_ADCOffset2;
-	  packet->sw_tHdr_unixTime = (packet->sw_tHdr_unixTime1 << 48) | (packet->sw_tHdr_unixTime2 << 32) | (packet->sw_tHdr_unixTime3 << 16) | packet->sw_tHdr_unixTime4;
-	};};
-
-  void update_sHdr_members(STMDataPacket &packet){
-	packet->s w_sHdr_sNum1 = &sw_sHdr[0];
-	packet->sw_sHdr_sNum2 = &sw_sHdr[1];
-	packet->sw_sHdr_sSize1 = &sw_sHdr[2];
-	packet->sw_sHdr_sSize2 = &sw_sHdr[3];
-	packet->sw_sHdr_ADCtime1 = &sw_sHdr[4];
-	packet->sw_sHdr_ADCtime2 = &sw_sHdr[5];
-	packet->sw_sHdr_ADCtime3 = &sw_sHdr[6];
-	packet->sw_sHdr_ADCtime4 = &sw_sHdr[7];
-	// Recombine words
-	if(packet->recombine){
-	  packet->sw_sHdr_sliceNumber = (packet->sw_sHdr_sNum1 << 16) | packet->sw_sHdr_sNum2;
-	  packet->sw_sHdr_sliceSize = (packet->sw_sHdr_sSize1 << 16) | packet->sw_sHdr_sSize2;
-	  packet->sw_sHdr_ADCtime = (packet->sw_sHdr_ADCtime1 << 48) | (packet->sw_sHdr_ADCtime2 << 32) | (packet->sw_sHdr_ADCtime3 << 16) | packet->sw_sHdr_ADCtime4;
-	};};
-*/
 
 #endif  // MU2E_ARTDAQ_CORE_OVERLAYS_STMFRAGMENT_HH
