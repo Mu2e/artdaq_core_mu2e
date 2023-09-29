6ee53029ea53d3dec8a37ee7256193ad69ac2101
Eric Flumerfelt <eflumerf@fnal.gov>
2023-09-29 16:20:28 -0500

    Remove Data Decoders from art Dictionary

--- artdaq-core-mu2e/Data/classes.h
+++ artdaq-core-mu2e/Data/classes.h
@@ -4,9 +4,4 @@
 #include "artdaq-core-mu2e/Data/EventHeader.hh"
 #include "artdaq-core-mu2e/Data/Mu2eEventHeader.hh"
 
-#include "artdaq-core-mu2e/Data/DTCDataDecoder.hh"
-#include "artdaq-core-mu2e/Data/TrackerDataDecoder.hh"
-#include "artdaq-core-mu2e/Data/CalorimeterDataDecoder.hh"
-#include "artdaq-core-mu2e/Data/CRVDataDecoder.hh"
-
 #include "canvas/Persistency/Common/Wrapper.h"
