6ec7bfa1f40bc2883f864598e0e12ebaeb43f0c1
RYAN A RIVERA <harthur@FNAL.GOV>
2025-01-28 05:16:44 -0600

    formatting

--- artdaq-core-mu2e/Overlays/DTC_Types/DTC_Link_ID.h
+++ artdaq-core-mu2e/Overlays/DTC_Types/DTC_Link_ID.h
@@ -9,52 +9,52 @@ namespace DTCLib {
 
 enum DTC_Link_ID : uint8_t
 {
-	DTC_Link_0 = 0,
-	DTC_Link_1 = 1,
-	DTC_Link_2 = 2,
-	DTC_Link_3 = 3,
-	DTC_Link_4 = 4,
-	DTC_Link_5 = 5,
-	DTC_Link_CFO = 6,
-	DTC_Link_EVB = 7,
-	DTC_Link_Unused,
-	DTC_Link_ALL = 255
+        DTC_Link_0 = 0,
+        DTC_Link_1 = 1,
+        DTC_Link_2 = 2,
+        DTC_Link_3 = 3,
+        DTC_Link_4 = 4,
+        DTC_Link_5 = 5,
+        DTC_Link_CFO = 6,
+        DTC_Link_EVB = 7,
+        DTC_Link_Unused,
+        DTC_Link_ALL = 255
 };
 
 inline std::ostream& operator<<(std::ostream& o, DTC_Link_ID const& link)
 {
-	return o << static_cast<unsigned int>(link);
+        return o << static_cast<unsigned int>(link);
 }
 
 // Prefix increment
-inline DTC_Link_ID& operator++(DTC_Link_ID& id) 
+inline DTC_Link_ID& operator++(DTC_Link_ID& id)
 {
-    if (id == DTC_Link_ALL || id == DTC_Link_Unused) 
-		throw std::runtime_error("Illegal ++ operator on DTC_Link_ID of value " +
-			std::to_string(id));
+    if (id == DTC_Link_ALL || id == DTC_Link_Unused)
+                throw std::runtime_error("Illegal ++ operator on DTC_Link_ID of value " +
+                        std::to_string(id));
 
     id = static_cast<DTC_Link_ID>(static_cast<uint8_t>(id) + 1);
     return id;
 } //end ++op
 
 // Postfix increment
-inline DTC_Link_ID operator++(DTC_Link_ID& id, int) 
+inline DTC_Link_ID operator++(DTC_Link_ID& id, int)
 {
-    if (id == DTC_Link_ALL || id == DTC_Link_Unused) 
-		throw std::runtime_error("Illegal ++ operator on DTC_Link_ID of value " +
-			std::to_string(id));
-			
+    if (id == DTC_Link_ALL || id == DTC_Link_Unused)
+                throw std::runtime_error("Illegal ++ operator on DTC_Link_ID of value " +
+                        std::to_string(id));
+
     DTC_Link_ID old = id;
     id = static_cast<DTC_Link_ID>(static_cast<uint8_t>(id) + 1);
     return old;
 } //end op++
 
 static const std::vector<DTC_Link_ID> DTC_ROC_Links{DTC_Link_0,
-													DTC_Link_1,
-													DTC_Link_2,
-													DTC_Link_3,
-													DTC_Link_4,
-													DTC_Link_5};
+                                                                                                        DTC_Link_1,
+                                                                                                        DTC_Link_2,
+                                                                                                        DTC_Link_3,
+                                                                                                        DTC_Link_4,
+                                                                                                        DTC_Link_5};
 
 }  // namespace DTCLib
 
