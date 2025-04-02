fe4d7e8bc8696d6e2f932001b232b218c5b2381f
mmackenz@FNAL.GOV <mmackenz@FNAL.GOV>
2025-04-02 16:49:02 -0500

    clang-format changes

--- artdaq-core-mu2e/Data/EventHeader.hh
+++ artdaq-core-mu2e/Data/EventHeader.hh
@@ -93,6 +93,6 @@ struct EventHeader
 std::ostream& operator<<(std::ostream& os,
 						 EventHeader const& eh);
 
-  typedef std::vector<mu2e::EventHeader> EventHeaders;
+typedef std::vector<mu2e::EventHeader> EventHeaders;
 }  // namespace mu2e
 #endif /* mu2e_artdaq_core_Data_EventHeader_hh */
