ae8b59606fe60b655b72eee68535fc314e915241
matthewstortini <matthewstortini@gmail.com>
2025-06-26 09:55:25 -0500

    updating struct

--- artdaq-core-mu2e/Overlays/Decoders/MTPDataDecoder.hh
+++ artdaq-core-mu2e/Overlays/Decoders/MTPDataDecoder.hh
@@ -13,20 +13,34 @@ public:
 
         explicit MTPDataDecoder(DTCLib::DTC_SubEvent const& evt);
 
-	struct MTPDataPacket
-	{
-                uint16_t timeStamp0;
-                uint16_t ewt0a;
-                uint16_t ewt0b;
-                uint16_t ewt0c;
-                uint16_t timeStamp1;
-                uint16_t ewt1a;
-                uint16_t ewt1b;
-                uint16_t ewt1c;
+        struct MTPDataPacket
+        {
+                 uint16_t timeStamp0;
+                 uint16_t ewt0a;
+                 uint16_t ewt0b;
+                 uint16_t ewt0c;
+                 uint16_t timeStamp1;
+                 uint16_t ewt1a;
+                 uint16_t ewt1b;
+                 uint16_t ewt1c;
 
-		MTPDataPacket()
-                  : timeStamp(0), ewt0(0), timeStamp1(0), ewt1(0) {}
-	};
+
+                 MTPDataPacket()
+                   : timeStamp0(0), ewt0a(0), ewt0b(0), ewt0c(0),
+                     timeStamp1(0), ewt1a(0), ewt1b(0), ewt1c(0)
+                 {}
+
+
+                 uint16_t GetTimestamp(int index) const
+                 {
+                         if (index == 0)
+                           return timeStamp0;
+                         else if (index == 1)
+                           return timeStamp1;
+                         else
+                           return 0;
+                 }
+        };
 
         typedef std::vector<const MTPDataPacket*> mtp_data_t;
 
