39a0ec821e24a5b400d5cc9718f07bc28dff9e6c
ehrlich-uva <ehrlich@virginia.edu>
2025-07-31 14:15:11 -0500

    assume CRV waveforms are unsigned

--- artdaq-core-mu2e/Overlays/Decoders/CRVDataDecoder.cc
+++ artdaq-core-mu2e/Overlays/Decoders/CRVDataDecoder.cc
@@ -87,13 +87,14 @@ bool mu2e::CRVDataDecoder::GetCRVHitsFEBII(size_t blockIndex, std::vector<CRVHit
 			++adcBlockPtr;
 		}
 		data += nADCblocks * sizeof(CRVHitADCBlockFEBII);
-
+/*
 		// handle negative numbers stored in 12bit ADC samples
 		for (size_t i = 0; i < waveform.size(); ++i)
 		{
 			// if bit 11 is 1 (from 12 bit ADC sample), set bits 12,13,14,14 to 1 to make it a negative number
 			if ((waveform[i] & 0x800) == 0x800) waveform[i] = (int16_t)(waveform[i] | 0xF000);
 		}
+*/
 	}
 
 	return true;
