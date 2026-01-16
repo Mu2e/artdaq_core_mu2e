fbbe1dc044ff505858ed76a25872c8cc4b6db8f4
Andrew Edmonds <aedmonds@york.cuny.edu>
2026-01-16 11:22:26 -0600

    Fix whitespace and hardcode header sizes for time being

--- artdaq-core-mu2e/Overlays/STMFragment.hh
+++ artdaq-core-mu2e/Overlays/STMFragment.hh
@@ -24,54 +24,60 @@
 //   static const uint DataBegin = 3;
 // };
 
+// TODO: get this from the authoritative source
+
 namespace mu2e {
 class STMFragment
 {
 public:
-	// static const fw_tHdr tHdr;
+  static constexpr size_t RAW_HEADER_LEN = 32; // length in words of RAW header
+  static constexpr size_t ZS_HEADER_LEN  = 6; // length in words of ZS header
+  static constexpr size_t MWD_HEADER_LEN = 4; // length in words of MWD header
 
-	explicit STMFragment(artdaq::Fragment const& f)
-		: artdaq_fragment_(f) {}
+        // static const fw_tHdr tHdr;
 
-	int16_t const* GetTHdr() const
-	{
-		return reinterpret_cast<int16_t const*>(artdaq_fragment_.dataBegin());
-	}
+        explicit STMFragment(artdaq::Fragment const& f)
+                : artdaq_fragment_(f) {}
 
-	int16_t const* detID() const
-	{
-		// return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.Ch_DTCclk_0);
-		return reinterpret_cast<int16_t const*>(GetTHdr() + 28);
-	}
+        int16_t const* GetTHdr() const
+        {
+                return reinterpret_cast<int16_t const*>(artdaq_fragment_.dataBegin());
+        }
 
-	int16_t const* EvNum() const
-	{
-		// return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.EvNum_0);
-		return reinterpret_cast<int16_t const*>(GetTHdr() + 8);
-	}
+        int16_t const* detID() const
+        {
+                // return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.Ch_DTCclk_0);
+                return reinterpret_cast<int16_t const*>(GetTHdr() + 28);
+        }
 
-	int16_t const* DataType() const
-	{
-		// return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.ZSflag_PreVal);
-		// return reinterpret_cast<int16_t const*>(GetTHdr()+22);
+        int16_t const* EvNum() const
+        {
+                // return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.EvNum_0);
+                return reinterpret_cast<int16_t const*>(GetTHdr() + 8);
+        }
 
-		// Dont have data type in Header yet for now assume it is Raw data
-		return reinterpret_cast<int16_t const*>(GetTHdr() + 22);
-	}
+        int16_t const* DataType() const
+        {
+                // return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.ZSflag_PreVal);
+                // return reinterpret_cast<int16_t const*>(GetTHdr()+22);
 
-	int16_t const* EvLen() const
-	{
-		// return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.EvLen);
-		return reinterpret_cast<int16_t const*>(GetTHdr() + 23);
-	}
+                // Dont have data type in Header yet for now assume it is Raw data
+                return reinterpret_cast<int16_t const*>(GetTHdr() + 22);
+        }
 
-	int16_t const* DataBegin() const
-	{
-		return reinterpret_cast<int16_t const*>(GetTHdr() + 32);
-	}
+        int16_t const* EvLen() const
+        {
+                // return reinterpret_cast<int16_t const*>(GetTHdr()+tHdr.EvLen);
+                return reinterpret_cast<int16_t const*>(GetTHdr() + 23);
+        }
+
+        int16_t const* DataBegin() const
+        {
+                return reinterpret_cast<int16_t const*>(GetTHdr() + 32);
+        }
 
 private:
-	artdaq::Fragment const& artdaq_fragment_;
+        artdaq::Fragment const& artdaq_fragment_;
 };
 
 }  // namespace mu2e
