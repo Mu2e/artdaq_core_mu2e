a365c696a097e1ac7d1d5fa02253ceee702a2d7b
Simon Corrodi <scorrodi@anl.gov>
2026-07-18 15:57:21 -0500

    clangify

--- artdaq-core-mu2e/Overlays/DTC_Packets/DTC_SubEvent.cpp
+++ artdaq-core-mu2e/Overlays/DTC_Packets/DTC_SubEvent.cpp
@@ -24,8 +24,9 @@ DTCLib::DTC_SubEvent::DTC_SubEvent(const void *data)
 		std::stringstream ss;
 		for (size_t i = 0; i < sizeof(header_); i += 4)
 			ss << std::dec << "#" << std::setw(2) << std::setfill('0') << i << "/" << sizeof(header_) << ": 0x" << std::hex << std::setw(8) << std::setfill('0') << *((uint32_t *)(&(ptr[i]))) << std::endl;
-		
-		TLOG(TLVL_ERROR) << "SUBEVENT FORMAT VERSION MISMATCH - Subevent header raw data:\n" << ss.str();
+
+		TLOG(TLVL_ERROR) << "SUBEVENT FORMAT VERSION MISMATCH - Subevent header raw data:\n"
+						 << ss.str();
 
 		TLOG(TLVL_ERROR) << "A DTC_WrongPacketTypeException occurred while setting up a DTC Subevent in the header format version 0x" << std::hex << header_.subevent_format_version << " != 0x" << static_cast<uint16_t>(REQUIRED_SUBEVENT_FORMAT_VERSION) << ". Check that your DTC FPGA version matches the software expectation.";
 		throw DTC_WrongPacketTypeException(REQUIRED_SUBEVENT_FORMAT_VERSION, header_.subevent_format_version);
